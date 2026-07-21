353aecac373190e75eef7542f02c66b62c530ff6
zitcon18 <kimb2304062@student.ctu.edu.vn>
2026-07-21 12:12:07 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,6 +3,12 @@ USE crypto_demo;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50),
-    password VARCHAR(255)
+    fullname VARCHAR(100) NOT NULL,
+    birthdate DATE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    phone TEXT,
+    citizen_id TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
